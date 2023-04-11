df7ecf6254c960fa7202df8c9e597482fba78bbe
Mo Chahadeh <applednd2002@gmail.com>
2023-04-12 01:05:55 +0300

    Report done until part 4

--- part2/part2a.v
+++ part2/part2a.v
@@ -1,23 +1,17 @@
-
-
-module part2a(input clk, input[7:0] I, input [1:0] FunSel, input LH, input enable, output reg [15:0] data_out);
+module part2a_IRreg(input clk, input[7:0] I, input [1:0] FunSel, input LH, input enable, output reg [15:0] data_out);
 
     always @(posedge clk)
     begin
         if (enable == 0)
             data_out = data_out;
         else
-            case (FunSel)
-                2'b00: data_out = 16'b000000000000000;
-                2'b01: 
-                case (LH)
-                    0: data_out[7:0] = I;
-                    1: data_out[15:8] = I;
-                endcase
-                2'b10: data_out = data_out - 1;
-                2'b11: data_out = data_out + 1;
-            endcase
+            if(FunSel == 2'b00) data_out = 16'b000000000000000;
+            else if(FunSel == 2'b01) begin
+                if(LH == 0) data_out[7:0] = I;
+                else if(LH == 1) data_out[15:8] = I;
+            end
+            else if(FunSel == 2'b10) data_out = data_out - 1;
+            else if(FunSel == 2'b11) data_out = data_out + 1;
     end
 
 endmodule
-
